98ce5ad810afc6a8f378a86cb1a001ba1966386d
Baraah <alsadibaraah@gmail.com>
2026-08-19 07:57:54 +0400

    add operators and comparisons section in warmups.js

--- Day4-Lab-task/Warmups/warmups.js
+++ Day4-Lab-task/Warmups/warmups.js
@@ -15,3 +15,21 @@ console.log(typeof value); // number
 
 value = "Hello";
 console.log(typeof value); // string
+
+// ------------------------------------------------------------
+// 2.2 Operators & Comparisons
+// ------------------------------------------------------------
+
+
+console.log("10" == 10);   // this is true
+console.log("10" === 10);  // this is false
+
+
+const userName = "";
+
+if (userName) {
+    console.log("Username exists");
+} else {
+    console.log("Username is empty");
+}
+
