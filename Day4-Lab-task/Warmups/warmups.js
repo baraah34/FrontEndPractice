c6179457be9066429f33d9d8e35cb19d93e68ea6
Baraah <alsadibaraah@gmail.com>
2026-08-19 07:54:30 +0400

    add variables and dynamic typing example in warmups.js

---
+++ Day4-Lab-task/Warmups/warmups.js
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------
+// 2.1 Variables & Dynamic Typing
+// ------------------------------------------------------------
+
+const myName = "Baraah";
+
+let value = 0;
+
+console.log("Name:", myName);
+
+console.log(typeof value); // number
+
+value = 10;
+console.log(typeof value); // number
+
+value = "Hello";
+console.log(typeof value); // string
